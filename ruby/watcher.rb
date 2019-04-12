632004c85983410eb9139d282ef3069c106bd38b
Jason A. Martin <JasonAMartin@users.noreply.github.com>
2019-04-12 08:16:42 -0400

    Update watcher.rb

--- ruby/watcher.rb
+++ ruby/watcher.rb
@@ -8,4 +8,12 @@ require 'filewatcher'
 Filewatcher.new('~/some-dir').watch do |filename, event|
   puts filename
   puts event
+  if filename.end_with? ".js"
+    # do somethinig for JS files
+    puts "Functional JS"
+  elsif filename.end_with? ".less"
+    # do something for LESS files
+    puts "CSS !important"
+  end
+
 end
