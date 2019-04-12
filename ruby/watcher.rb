4545ff137814b8a332a3f7be5aa88b1c38fc77c6
Jason A. Martin <JasonAMartin@users.noreply.github.com>
2019-04-12 08:32:34 -0400

    Update watcher.rb

--- ruby/watcher.rb
+++ ruby/watcher.rb
@@ -4,10 +4,17 @@
 # Docs: https://github.com/filewatcher/filewatcher
 
 require 'filewatcher'
+require 'pathname'
 
-Filewatcher.new('~/some-dir').watch do |filename, event|
+Filewatcher.new('~/temp/**/*.*', spinner: true, interval: 0.1).watch do |filename, event|
   puts filename
   puts event
+
+  path = Pathname.new(filename)
+  puts "Basename         : #{path.basename}"
+  puts "Relative filename: #{File.join('.', path)}"
+  puts "Absolute filename: #{path.realpath}"
+
   if filename.end_with? ".js"
     # do somethinig for JS files
     puts "Functional JS"
